216dcdc5dc05abf45d2b36bd052d25e853969036
Ryan D'Silva <ryan_dsilva@protonmail.com>
2021-09-13 01:28:24 -0400

    fix: add typehinting
    chore: add .drone.yml for ci

--- telemood_app/pkgs/blockDM.py
+++ telemood_app/pkgs/blockDM.py
@@ -1,5 +1,8 @@
-from pyrogram.raw.types.update_encryption import UpdateEncryption
-from pyrogram.raw.types import EncryptedChatRequested
+from pyrogram.raw.types.update_encryption import UpdateEncryption # type: ignore
+from pyrogram.raw.types import EncryptedChatRequested # type: ignore
+from pyrogram.types import User # type: ignore
+
+from typing import List
 
 async def block_regular_dm(telemood, unsolicited_message):
     if not (unsolicited_message.from_user.is_contact or unsolicited_message.from_user.is_self or unsolicited_message.from_user.is_bot):
@@ -8,8 +11,8 @@ async def block_regular_dm(telemood, unsolicited_message):
 
 async def block_secret_dm(telemood, update, users):
     if isinstance(update, UpdateEncryption) and isinstance(update["chat"], EncryptedChatRequested):
-        offender_id = list(users.keys())[0]
-        offender = await telemood.get_users(offender_id)
+        offender_id: List = list(users.keys())[0]
+        offender: User = await telemood.get_users(offender_id)
         if not offender.is_contact:
             await telemood.block_user(offender_id)
     pass
--- telemood_app/pkgs/getLastFMSong.py
+++ telemood_app/pkgs/getLastFMSong.py
@@ -1,12 +1,14 @@
+from typing import List 
+
 async def get_last_song(telemood):
-    last_song = (await telemood.get_inline_bot_results("lastfmrobot"))["results"][0]["send_message"]["message"]
-    name_tokens = (await telemood.get_me())["first_name"].split()
+    last_song: str = (await telemood.get_inline_bot_results("lastfmrobot"))["results"][0]["send_message"]["message"]
+    name_tokens: List[str] = (await telemood.get_me())["first_name"].split()
     if "is now listening to" in last_song:
         if not "🎵" in name_tokens:
             name_tokens.insert(0, "🎵")
             name_tokens.append( "🎵")
     else:
         if "🎵" in name_tokens:
-            name_tokens = list(name_tokens[1:-1])
+            name_tokens: List[str] = list(name_tokens[1:-1])
     new_name = " ".join(name_tokens)
     await telemood.update_profile(first_name = new_name)
